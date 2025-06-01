2c4af2b0e63ddf7485ec6c3ae8f40897c698ff2d
kyungminoh12 <janyoh1992@gmail.com>
2025-06-01 15:03:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+function shareToInstagram() {
+  alert("인스타그램 공유는 직접 스토리 업로드를 권장합니다. 이미지를 저장 후 업로드해주세요!");
+}
+
+function shareToFacebook() {
+  const url = encodeURIComponent(window.location.href);
+  window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`, '_blank');
+}
